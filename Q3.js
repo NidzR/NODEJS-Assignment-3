ca16f4d9a22ac98ab12250bcdb51d21c636319a5
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 02:32:34 +0500

    NODEJS Assignment Q3
    3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.

---
+++ Q3.js
@@ -0,0 +1,6 @@
+"use strict";
+// 2.Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let firstName = "Nida Rizwan";
+console.log(firstName.toLocaleLowerCase());
+console.log(firstName.toLocaleUpperCase());
+console.log(firstName.charAt(0).toLocaleUpperCase() + firstName.slice(1));
